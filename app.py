9a66886acbf23e733cb0e2f75ea5af15b798317c
Paruhang Angdembe <paruhanglimbu5@gmail.com>
2024-04-14 20:35:33 -0400

    working, includes the response.json aswell

--- app.py
+++ app.py
@@ -1,11 +1,62 @@
-import os
-from flask import Flask, request, jsonify
-from googleapiclient.discovery import build
-from transformers import pipeline
+from flask import Flask, request, jsonify, Response
+from flask_cors import CORS
+from transformers import pipeline, TFPegasusForConditionalGeneration, AutoTokenizer
 import pandas as pd
-from collections import Counter
+import os
+import re
+from googleapiclient.discovery import build
 
 app = Flask(__name__)
+CORS(app)
+
+classifier = pipeline(
+    task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None
+)
+
+# Initialize the summarization model and tokenizer
+tokenizer = None
+model = None
+cached_model = None
+cached_tokenizer = None
+
+
+def load_transformer_models():
+    global tokenizer, model, cached_tokenizer, cached_model
+
+    if cached_tokenizer and cached_model:
+        tokenizer = cached_tokenizer
+        model = cached_model
+        return
+
+    # Check if model and tokenizer are saved, if not, load from pretrained
+    if os.path.exists("saved_models/my_tokenizer"):
+        tokenizer = AutoTokenizer.from_pretrained(
+            "saved_models/my_tokenizer", local_files_only=True
+        )
+        model = TFPegasusForConditionalGeneration.from_pretrained(
+            "saved_models/my_model", local_files_only=True
+        )
+
+    cached_tokenizer = tokenizer
+    cached_model = model
+
+
+# Load transformer models during application startup
+# load_transformer_models()
+
+
+def get_max_score_label(data_list):
+    if not data_list:  # Check if the list is not empty
+        return None
+
+    max_label = max(data_list, key=lambda x: x["score"])
+    return max_label["label"]
+
+
+def preprocess_comments(text):
+    text = re.sub(r"http\S+", "", text)  # Remove URLs
+    text = text.encode("ascii", "ignore").decode("ascii")  # Remove non-ASCII characters
+    return text
 
 
 def get_youtube_comments(video_url, api_key):
@@ -30,7 +81,7 @@ def get_youtube_comments(video_url, api_key):
             .list(
                 part="snippet",
                 videoId=video_id,
-                maxResults=100,  # Adjust as needed
+                maxResults=100,
                 pageToken=nextPageToken,
             )
             .execute()
@@ -48,93 +99,94 @@ def get_youtube_comments(video_url, api_key):
     return video_title, comments
 
 
-classifier = pipeline(
-    task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None
-)
+@app.route("/analyze_comments", methods=["POST"])
+def analyze_comments():
 
-
-def process_model_output(model_output):
-    if not model_output:  # Check if the list is not empty
-        return None
-
-    # Initialize variables to store the maximum score and corresponding label
-    max_score = float("-inf")
-    max_label = None
-
-    # Iterate over each element in the model_output list
-    for sublist in model_output:
-        # If the element is not a list, skip it
-        if not isinstance(sublist, list):
-            continue
-
-        # Iterate over each dictionary in the sublist
-        for output in sublist:
-            # If the element is not a dictionary, skip it
-            if not isinstance(output, dict):
-                continue
-
-            # Extract the score and label from the current dictionary
-            score = output.get("score", 0.0)
-            label = output.get("label", "")
-
-            # Update max_score and max_label if the current score is greater
-            if score > max_score:
-                max_score = score
-                max_label = label
-
-    return max_label
-
-
-@app.route("/")
-def analyze_news():
-    return "hello world"
-
-
-@app.route("/analyze_sentiment", methods=["POST"])
-def analyze_sentiment():
-    # Extract video URL from the request
     video_url = request.json.get("video_url")
 
-    api_key = "asdf"
+    api_key = "AIzaSyDeCab2QGoqnTZi_wcMH8XdRqg6Hd91JI4"
 
-    # Perform sentiment analysis
     video_title, comments = get_youtube_comments(video_url, api_key)
     sentiment_results = []
 
-    for comment in comments:
-        model_output = classifier(comment)
-        sentiment_result = process_model_output(model_output)
-        sentiment_results.append(
-            (comment, sentiment_result)
-        )  # Include comment with sentiment result
+    df = pd.DataFrame(columns=["SerialNo", "Label", "Comment"])
 
-    # Calculate sentiment summary
-    sentiment_counts = Counter(
-        sentiment_result for _, sentiment_result in sentiment_results
+    # Lazy Loading
+    if not tokenizer or not model:
+        load_transformer_models()
+
+    batch_results = []
+
+    # Batch Processing
+    batch_size = 10
+    num_batches = len(comments) // batch_size + 1
+
+    for batch_num in range(num_batches):
+        batch_comments = comments[batch_num * batch_size : (batch_num + 1) * batch_size]
+
+        for i, comment in enumerate(batch_comments, start=batch_num * batch_size + 1):
+            model_output = classifier(comment)
+            max_label = get_max_score_label(model_output[0])
+            batch_results.append(
+                {"SerialNo": i, "Label": max_label, "Comment": comment}
+            )
+
+    df = pd.DataFrame(batch_results)
+
+    print(df.info())
+    print(df)
+
+    # Convert sentiment counts to a list of dictionaries
+    sentiment_counts = df["Label"].value_counts().reset_index()
+    sentiment_counts.columns = ["Sentiment", "Count"]
+    sentiment_counts_list = sentiment_counts.to_dict(orient="records")
+
+    # Sort the DataFrame by label
+    df_sorted = df.sort_values(by="Label")
+    sorted_comments_list = df_sorted.to_dict(orient="records")
+
+    # Summarize comments by sentiment
+    comments_by_sentiment = df.groupby("Label")["Comment"].apply(" ".join).to_dict()
+    summary_by_sentiment = {}
+
+    for sentiment, comments in comments_by_sentiment.items():
+        clean_comments = preprocess_comments(comments)
+        inputs = tokenizer(
+            clean_comments, max_length=1024, truncation=True, return_tensors="tf"
+        )
+        summary_ids = model.generate(inputs["input_ids"])
+        summary_text = tokenizer.decode(
+            summary_ids[0].numpy(), skip_special_tokens=True
+        )
+        summary_by_sentiment[sentiment] = summary_text
+
+    # Return the results as JSON
+    return jsonify(
+        {
+            "video_title": video_title,
+            "sentiment_counts": sentiment_counts_list,
+            "sorted_comments": sorted_comments_list,
+            "summary_by_sentiment": summary_by_sentiment,
+        }
     )
-    total_comments = len(sentiment_results)
-    sentiment_summary = {
-        "positive": sentiment_counts.get("positive", 0),
-        "negative": sentiment_counts.get("negative", 0),
-        "neutral": sentiment_counts.get("neutral", 0),
-        "total_comments": total_comments,
-    }
 
-    # Calculate overall sentiment score
-    positive_comments = sentiment_counts.get("positive", 0)
-    negative_comments = sentiment_counts.get("negative", 0)
-    overall_sentiment_score = (positive_comments - negative_comments) / total_comments
 
-    # Construct the response
-    response = {
-        "video_title": video_title,
-        "sentiment_summary": sentiment_summary,
-        "overall_sentiment_score": overall_sentiment_score,
-        "sentiment_results": sentiment_results,  # Include comments with sentiment results
-    }
+@app.route("/download_csv", methods=["POST"])
+def download_csv():
+    data = request.json
+    df = pd.DataFrame(data["sorted_comments"])
+    csv = df.to_csv(index=False)
+    return Response(
+        csv,
+        mimetype="text/csv",
+        headers={"Content-disposition": "attachment; filename=comments_analysis.csv"},
+    )
 
-    return jsonify(response)
+
+@app.route("/home")
+def home():
+    return "hello"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=True)
